00536e7deddf521c68b15e09df940362b643b13e
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-09-04 16:58:00 +0600

    "commit"

--- lib/Screen/HomeScreen.dart
+++ lib/Screen/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ride_share_app/data/colors.dart';
 
 class HomeScreen extends   StatelessWidget {
   const HomeScreen({super.key});
@@ -6,6 +7,7 @@ class HomeScreen extends   StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.white ,
       body: Container(
 
       ),
