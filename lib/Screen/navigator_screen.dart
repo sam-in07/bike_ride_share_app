42cbbdb784008c74eb1b663522c5fbd1ee4eac7f
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-09-15 00:13:54 +0600

    "commit"

--- lib/Screen/navigator_screen.dart
+++ lib/Screen/navigator_screen.dart
@@ -1,1 +1,22 @@
 
+import 'package:flutter/material.dart';
+
+class naviscree extends StatelessWidget {
+  const naviscree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset("assets/images/maps.png"),
+        ],
+      ),
+
+    );
+  }
+
+}
